ca4f3d0a157489609473952d74572ef88e9b0ec4
Karthik Amaram <amaramkart09@gmail.com>
2025-07-10 11:24:15 -0500

    Added a test and passed the test

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from './components/Homepage.jsx'
+import Shop from './components/Shop.jsx'
+import Cart from './components/Cart.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    children: [
+      { index: true, element: <Homepage />},
+      { path: "shop", element: <Shop />},
+      { path: "cart", element: <Cart />},
+    ]
   }
 ])
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
